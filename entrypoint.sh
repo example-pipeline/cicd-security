b978933d89291e5304eb6995a8b40cadd70c2b98
AndrewHiron <andrewh@elderda.co.uk>
2023-07-19 10:53:39 +0000

    trivy SBOM output as SARIF

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ cd $GITHUB_WORKSPACE
 echo "Running grype scan..."
 grype -o sarif --file grype.sarif $GITHUB_WORKSPACE
 
-echo "Creating trivy SBOM file"
-trivy filesystem --format cyclonedx --output /app/result.json .
-echo "Running trivy SBOM scan"
-trivy sbom --offline-scan --skip-db-update --skip-java-db-update /app/result.json
-
 echo "Running trivy file system scan"
-trivy sbom --offline-scan --skip-db-update --skip-java-db-update /app/result.json
+trivy filesystem --format sarif /app --offline-scan --skip-db-update --skip-java-db-update --output /app/filesystem.sarif
+
+echo "Creating cyclonedx file for trivy SBOM scan"
+trivy filesystem --format cyclonedx /app --output /app/filesystem.cyclonedx  # Does not require the database
+echo "Running trivy SBOM scan"
+trivy sbom --offline-scan --skip-db-update --skip-java-db-update --output /app/filesystemsbom.sarif /app/filesystem.cyclonedx
 
 echo "exiting ..."
