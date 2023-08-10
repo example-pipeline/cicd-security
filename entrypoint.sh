8fd7c322b89734263224b9dbb83d6bdf74fbb9df
AndyHiron <56255148+AndyHiron@users.noreply.github.com>
2023-08-10 13:39:28 +0100

    Update entrypoint.sh so trivy writes to local folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ echo "Running grype scan..."
 grype -o sarif --file grype.sarif $GITHUB_WORKSPACE
 
 echo "Running trivy file system scan"
-trivy filesystem --format sarif /app --offline-scan --skip-db-update --skip-java-db-update --output /app/filesystem.sarif
+trivy filesystem --format sarif /app --offline-scan --skip-db-update --skip-java-db-update --output trivyfilesystem.sarif
 
 echo "Creating cyclonedx file for trivy SBOM scan"
-trivy filesystem --format cyclonedx /app --output /app/filesystem.cyclonedx  # Does not require the database
+trivy filesystem --format cyclonedx /app --output trivyfilesystem.cyclonedx  # Does not require the database
 echo "Running trivy SBOM scan"
-trivy sbom --offline-scan --skip-db-update --skip-java-db-update --output /app/filesystemsbom.sarif /app/filesystem.cyclonedx
+trivy sbom --offline-scan --skip-db-update --skip-java-db-update --output trivyfilesystemsbom.sarif trivyfilesystem.cyclonedx
 
 echo "Running trivy repo scan for " $GITHUB_REPO_URL
 trivy repo --offline-scan --skip-db-update --skip-java-db-update $GITHUB_REPO_URL
